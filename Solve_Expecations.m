18e40cf9fd79bf0cb684f7f111d9ea3b2d0c2f73
Sarunas-Girdenas <sg325@exeter.ac.uk>
2016-01-29 16:37:27 +0000

    Updated Version for Speed

--- Solve_Expecations.m
+++ Solve_Expecations.m
@@ -25,9 +25,21 @@ function [ E_K, E_S ] = Solve_Expecations( t, SteadyStateValuesNK, ParameterValu
         
         % changed B1^i to exp(i*log(B1)) to speed up the code
         
-        tmp1 = [ FirmsParameters.inflation_Param(2,t) FirmsParameters.inflation_Param(3,t) ]*(exp(i*log(B1)))*[ ActualLawOfMotion.capital(1,t)-SteadyStateValuesNK.k ActualLawOfMotion.A(1,t)-SteadyStateValuesNK.A ]';
-        tmp2 = [ FirmsParameters.markup_Param(2,t) FirmsParameters.markup_Param(3,t) ]*(exp(i*log(B1)))*[ ActualLawOfMotion.capital(1,t)-SteadyStateValuesNK.k ActualLawOfMotion.A(1,t)-SteadyStateValuesNK.A ]';
-        E_K  = (ParameterValues.theta*ParameterValues.beta)^i*tmp1-(1-ParameterValues.theta*ParameterValues.beta)/SteadyStateValuesNK.X*(ParameterValues.theta*ParameterValues.beta)^i*tmp2+E_K;
+        if i == 1
+            
+            B1_i        = B1;
+            betaTheta_i = ParameterValues.beta*ParameterValues.theta;
+            
+        else
+            
+            B1_i        = B1_i * B1;
+            betaTheta_i = betaTheta_i * ParameterValues.beta*ParameterValues.theta;
+            
+        end
+        
+        tmp1 = [ FirmsParameters.inflation_Param(2,t) FirmsParameters.inflation_Param(3,t) ]*(B1_i)*[ ActualLawOfMotion.capital(1,t)-SteadyStateValuesNK.k ActualLawOfMotion.A(1,t)-SteadyStateValuesNK.A ]';
+        tmp2 = [ FirmsParameters.markup_Param(2,t) FirmsParameters.markup_Param(3,t) ]*(B1_i)*[ ActualLawOfMotion.capital(1,t)-SteadyStateValuesNK.k ActualLawOfMotion.A(1,t)-SteadyStateValuesNK.A ]';
+        E_K  = (betaTheta_i)*tmp1-(1-ParameterValues.theta*ParameterValues.beta)/SteadyStateValuesNK.X*(betaTheta_i)*tmp2+E_K;
   
     end
 
@@ -38,16 +50,30 @@ function [ E_K, E_S ] = Solve_Expecations( t, SteadyStateValuesNK, ParameterValu
     tmp4 = 0;
     tmp5 = 0;
     
-    % second sum
+    % second sum    
     
     for i = 1:ParameterValues.forecastPeriod
+        
+        if i == 1
+            
+            B2_i   = B2;
+            B1_i   = B1;
+            beta_i = ParameterValues.beta;
+            
+        else
+            
+            B2_i   = B2_i * B2;
+            B1_i   = B1_i * B1;
+            beta_i = beta_i * ParameterValues.beta;
+            
+        end
 
-        tmp1 = (exp(i*log(B2)))*[ActualLawOfMotion.capital(1,t)-SteadyStateValuesNK.k ActualLawOfMotion.A(1,t)-SteadyStateValuesNK.A]';
-        tmp2 = [HouseholdParameters.interestRate_Param(2,t) HouseholdParameters.interestRate_Param(3,t)]*(exp(i*log(B2)))*[ActualLawOfMotion.capital(1,t)-SteadyStateValuesNK.k ActualLawOfMotion.A(1,t)-SteadyStateValuesNK.A]';
-        tmp3 = [HouseholdParameters.inflation_Param(2,t) HouseholdParameters.inflation_Param(3,t)]*(exp(i*log(B2)))*[ActualLawOfMotion.capital(1,t)-SteadyStateValuesNK.k ActualLawOfMotion.A(1,t)-SteadyStateValuesNK.A]';
-        tmp4 = [HouseholdParameters.wage_Param(2,t) HouseholdParameters.wage_Param(3,t)]*(exp(i*log(B2)))*[ActualLawOfMotion.capital(1,t)-SteadyStateValuesNK.k ActualLawOfMotion.A(1,t)-SteadyStateValuesNK.A]';
-        tmp5 = [HouseholdParameters.markup_Param(2,t) HouseholdParameters.markup_Param(3,t)]*(exp(i*log(B1)))*[ActualLawOfMotion.capital(1,t)-SteadyStateValuesNK.k ActualLawOfMotion.A(1,t)-SteadyStateValuesNK.A]';
-        E_S  = ParameterValuesLearning.Ck/(1-ParameterValues.beta)*(exp(i*log(ParameterValues.beta)))*tmp1(1,1)+ParameterValuesLearning.Ca/(1-ParameterValues.beta)*(exp(i*log(ParameterValues.beta)))*tmp1(2,1) + (SteadyStateValuesNK.c*ParameterValuesLearning.Cc*ParameterValues.beta/(1-ParameterValues.beta)^2-SteadyStateValuesNK.M*ParameterValues.beta/SteadyStateValuesNK.R/(1-ParameterValues.beta))*(exp(i*log(ParameterValues.beta)))*tmp2+(SteadyStateValuesNK.M/(1-ParameterValues.beta)-SteadyStateValuesNK.c*ParameterValuesLearning.Cc/(1-ParameterValues.beta)^2)*(exp(i*log(ParameterValues.beta)))*tmp3+ParameterValuesLearning.Cw/(1-ParameterValues.beta)*(exp(i*log(ParameterValues.beta)))*tmp4+ParameterValuesLearning.Cx/(1-ParameterValues.beta)*(exp(i*log(ParameterValues.beta)))*tmp5+E_S;
+        tmp1 = (B2_i)*[ActualLawOfMotion.capital(1,t)-SteadyStateValuesNK.k ActualLawOfMotion.A(1,t)-SteadyStateValuesNK.A]';
+        tmp2 = [HouseholdParameters.interestRate_Param(2,t) HouseholdParameters.interestRate_Param(3,t)]*(B2_i)*[ActualLawOfMotion.capital(1,t)-SteadyStateValuesNK.k ActualLawOfMotion.A(1,t)-SteadyStateValuesNK.A]';
+        tmp3 = [HouseholdParameters.inflation_Param(2,t) HouseholdParameters.inflation_Param(3,t)]*(B2_i)*[ActualLawOfMotion.capital(1,t)-SteadyStateValuesNK.k ActualLawOfMotion.A(1,t)-SteadyStateValuesNK.A]';
+        tmp4 = [HouseholdParameters.wage_Param(2,t) HouseholdParameters.wage_Param(3,t)]*(B2_i)*[ActualLawOfMotion.capital(1,t)-SteadyStateValuesNK.k ActualLawOfMotion.A(1,t)-SteadyStateValuesNK.A]';
+        tmp5 = [HouseholdParameters.markup_Param(2,t) HouseholdParameters.markup_Param(3,t)]*(B1_i)*[ActualLawOfMotion.capital(1,t)-SteadyStateValuesNK.k ActualLawOfMotion.A(1,t)-SteadyStateValuesNK.A]';
+        E_S  = ParameterValuesLearning.Ck/(1-ParameterValues.beta)*(beta_i)*tmp1(1,1)+ParameterValuesLearning.Ca/(1-ParameterValues.beta)*(beta_i)*tmp1(2,1)+(SteadyStateValuesNK.c*ParameterValuesLearning.Cc*ParameterValues.beta/(1-ParameterValues.beta)^2-SteadyStateValuesNK.M*ParameterValues.beta/SteadyStateValuesNK.R/(1-ParameterValues.beta))*(beta_i)*tmp2+(SteadyStateValuesNK.M/(1-ParameterValues.beta)-SteadyStateValuesNK.c*ParameterValuesLearning.Cc/(1-ParameterValues.beta)^2)*(beta_i)*tmp3+ParameterValuesLearning.Cw/(1-ParameterValues.beta)*(beta_i)*tmp4+ParameterValuesLearning.Cx/(1-ParameterValues.beta)*(beta_i)*tmp5+E_S;
      
     end
     
